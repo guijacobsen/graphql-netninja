f4ee2e124bf4c2d8bb3921736e365b766126537e
Guilherme Jacobsen <guijacobsen@gmail.com>
2020-09-16 02:18:49 -0300

    lists of data types

--- src/schema/index.js
+++ src/schema/index.js
@@ -6,6 +6,7 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLID,
+  GraphQLList,
 } = graphql;
 
 // moked data
@@ -13,6 +14,9 @@ let books = [
   { name: "Livro 1", genre: "Genre 1", id: 1, authorId: 1 },
   { name: "Livro 2", genre: "Genre 2", id: 2, authorId: 1 },
   { name: "Livro 3", genre: "Genre 3", id: 3, authorId: 2 },
+  { name: "Livro 4", genre: "Genre 4", id: 4, authorId: 2 },
+  { name: "Livro 5", genre: "Genre 5", id: 5, authorId: 3 },
+  { name: "Livro 6", genre: "Genre 6", id: 6, authorId: 3 },
 ];
 let authors = [
   { name: "Author 1", age: 21, id: 1 },
@@ -27,6 +31,13 @@ const AuthorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        console.log("-- parent : ", parent);
+        return books.filter((book) => book.authorId == parent.id);
+      },
+    },
   }),
 });
 
@@ -40,7 +51,7 @@ const BookType = new GraphQLObjectType({
       type: AuthorType,
       resolve(parent, args) {
         console.log("-- parent : ", parent);
-        return authors.filter((i) => i.id == parent.authorId)[0];
+        return authors.filter((author) => author.id == parent.authorId)[0];
       },
     },
   }),
@@ -50,14 +61,26 @@ const BookType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      },
+    },
     book: {
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db / other source
         console.log(typeof args.id);
-        const book = books.filter((i) => i.id == args.id);
+        const book = books.filter((book) => book.id == args.id);
         return book[0] || null;
+      },
+    },
+    authors: {
+      type: GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
       },
     },
     author: {
@@ -65,7 +88,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         console.log(typeof args.id);
-        return authors.filter((i) => i.id == args.id)[0];
+        return authors.filter((author) => author.id == args.id)[0];
       },
     },
   },
